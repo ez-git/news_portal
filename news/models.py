45a38f5560ca9565c011fab49e2f4dc0e0008431
EZ <ezaytsev.main@gmail.com>
2022-10-09 20:25:36 +0300

    templates and views + unversioned

--- news/models.py
+++ news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True) # Не обязательно к заполнению no need to fill
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+# id - int - django will create it auto if primary key not found
+# title - varchar
+# content text
+# created_at
+# updated_at - datetime
+# photo - image
+# is_publish - bool
--- news/views.py
+++ news/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {'news': news,
+               'title': 'News list'}
+
+
+    return render(request, 'news/index.html', context)
+    # res = '<h1>Список новостей</h1>'
+    # for item in news:
+    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+    # return HttpResponse(res)
 
 def test(request):
     # print(dir(request))
